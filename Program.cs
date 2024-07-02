65febf7e35bd6557a185af9f81104965fe432897
alihassan1177 <ah927481@gmail.com>
2024-07-02 07:08:54 -0700

    fetching html from timesjobs.com

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+HttpClient client = new();
+string html = await client.GetStringAsync("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=");
+html = Regex.Replace(html, @"^\s+|\s+$", "", RegexOptions.Multiline);
+html = Regex.Replace(html, @"\s{2,}", " ");
+
+HtmlDocument document = new();
+document.LoadHtml(html);
+
+var joblist = document.DocumentNode.SelectSingleNode("//ul[@class='new-joblist']");
+foreach (var listItem in joblist.ChildNodes)
+{
+    if (listItem.InnerHtml.Replace(" ", "").Trim() != string.Empty)
+    {
+        Console.WriteLine($"List Item : {listItem.InnerHtml}");
+    };
+}
